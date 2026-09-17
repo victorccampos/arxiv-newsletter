2918c10ef62116aa34b32fac37c1e2b7fee6c14b
victocampos@nitroV <victorjvc2020@gmail.com>
2026-09-17 16:23:14 -0300

    improve methods readability

--- src/main.py
+++ src/main.py
@@ -1,29 +1,19 @@
-from typing import Any
-from fetch_arxiv_papers import search_candidates, write_papers_query
-from gemini_select import select_papers
-from send_newsletter_email import send_email, clean_markdown_response
+from fetch_arxiv_papers import search_candidates, write_papers_list
+from gemini_select import select_papers, load_prompt
+from send_newsletter_email import send_email, format_html
 
 def main():
-    # Buscar papers do Arxiv
-    print("Looking for papers in ArXiv...")
-    papers: list[dict[str, Any]] = search_candidates(category="cond-mat.mtrl-sci", n_papers=10)
+    # Buscar papers candidatos do Arxiv
+    papers = search_candidates(category="physics.comp-ph", candidate_papers=20)
     
-    # Escrever a query do Arxiv 
-    print("Writing paper query to insert in Gemini prompt.")
-    papers_query: str = write_papers_query(papers, write=True)
-
-    # Selecionar os 3 melhores papers com base no prompt
-    print("Selecting 3 best papers to create newsletter content.")
-    markdown_text: str = select_papers(papers_query=papers_query)
-
-    html_content = clean_markdown_response(markdown_text)
+    # Monta o prompt completo com a lista de candidatos
+    prompt = load_prompt("../prompts/prompt.txt") + write_papers_list(papers)
     
-    with open("newsletter_content.html", "w") as f:
-        f.write(html_content)
-
-
+    # Selecionar os papers com Gemini
+    html_newsletter = format_html(select_papers(prompt))
+    
     # Enviar e-mail
-    send_email(subject="Newsletter do JVC", html_content=html_content)
+    send_email(subject="Newsletter do JVC", html_content=html_newsletter)
 
 
 if __name__ == "__main__":
